4b5e07871096fb7028fcee4b077e22f540c37034
Sahil <sahilchopra1975@gmail.com>
2025-07-08 23:17:22 +0530

    fixed the bug

--- services/PredictionService.js
+++ services/PredictionService.js
@@ -1,4 +1,5 @@
-const baseURL = "https://weather-forcating-backend.onrender.com/";
+const baseURL = "https://weather-forcating-backend.onrender.com";
+
 export const predictRainTomorrow = async (lat, lon) => {
   const res = await fetch(`${baseURL}/predict?latitude=${lat}&longitude=${lon}`);
   if (!res.ok) throw new Error("Prediction failed");
